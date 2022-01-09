40f3f10e64ca9dbcfd72aca99b20630cf15604ca
Himank Suiwala <himanksuiwala@gmail.com>
2022-01-09 12:27:28 +0530

    Added Dispatch actions for various providers

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import Banner from "./Banner";
 import axios from "axios";
 import RowContainer from "./RowContainer";
@@ -8,42 +8,57 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { setsearchData } from "./features/movie/searchSlice";
-import { fetchAsyncMovies ,fetchAsyncTV,fetchAsyncSearch,fetchAsyncPRIMEVIDEOS, fetchAsyncPRIMEVIDEOSm} from "./features/movie/movieSlice";
+import {
+  fetchAsyncMovies,
+  fetchAsyncTV,
+  fetchAsyncNETFLIXmovie,
+  fetchAsyncNETFLIXtv,
+  fetchAsyncPRIMEmovie,
+  fetchAsyncPRIMEtv,
+  fetchAsyncHOTSTARmovie,
+  fetchAsyncHOTSTARtv
+} from "./features/movie/movieSlice";
 const API_KEY = "74de71d03e661d70414e23b100e51515";
 
-export default function Home() { 
-     const [term,setTerm] = useState("");
-//   const submitHandler =(e)=>{
-//     e.preventDefault();
-//     fetchsearch(term);
-//   setTerm("");
-// }
+export default function Home() {
+  const [term, setTerm] = useState("");
+  //   const submitHandler =(e)=>{
+  //     e.preventDefault();
+  //     fetchsearch(term);
+  //   setTerm("");
+  // }
 
   const dispatch = useDispatch();
-  const fetchsearch = async(term)=>{
-    const response = await axios.get(`
+  const fetchsearch = async (term) => {
+    const response = await axios
+      .get(
+        `
   https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${term}&page=1&include_adult=false
-  `).catch((e)=>{
-      console.log("Error fetching Search Data");
-  })
- dispatch(setsearchData(response.data))
-}
+  `
+      )
+      .catch((e) => {
+        console.log("Error fetching Search Data");
+      });
+    dispatch(setsearchData(response.data));
+  };
 
   useEffect(() => {
-    dispatch(fetchAsyncTV());
+    dispatch(fetchAsyncNETFLIXtv());
+    dispatch(fetchAsyncNETFLIXmovie());
     dispatch(fetchAsyncMovies());
-    dispatch(fetchAsyncPRIMEVIDEOS());
-    dispatch(fetchAsyncPRIMEVIDEOSm());
+    dispatch(fetchAsyncPRIMEmovie());
+    dispatch(fetchAsyncPRIMEtv());
+    dispatch(fetchAsyncHOTSTARtv());
+    dispatch(fetchAsyncHOTSTARmovie());
+
     // fetchsearch();
     // dispatch(fetchAsyncSearch("Money"));
-    return () => {
-     
-    }
-  }, [dispatch])
+    return () => {};
+  }, [dispatch]);
   return (
     <>
       {/* <Banner /> */}
-      <SearchComponent/>
+      <SearchComponent />
       {/* <Main>
         <div classname ='container'>
         <h1>Search component</h1>
@@ -70,12 +85,12 @@ export default function Home() {
 }
 
 const Main = styled.div`
-background-color:red;
-color:white;
-height:300px;
-.container{
-    color:green;
-    padding:100px;
-}
-${'' /* margin-top:50px; */}
+  background-color: red;
+  color: white;
+  height: 300px;
+  .container {
+    color: green;
+    padding: 100px;
+  }
+  ${"" /* margin-top:50px; */}
 `;
